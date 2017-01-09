2027ef84ddd34b667b84dc9dffb325670ca67484
Kotwic4 <Kotwic4@gmail.com>
2017-01-09 20:47:20 +0100

    add more simple tests

--- src/test/java/agh/cs/lab9/OptionParserTest.java
+++ src/test/java/agh/cs/lab9/OptionParserTest.java
@@ -2,6 +2,9 @@ package agh.cs.lab9;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 import static org.junit.Assert.*;
 
 public class OptionParserTest {
@@ -15,6 +18,36 @@ public class OptionParserTest {
         assertEquals(OptionType.lenghtTrip, optionParser.parseOption("7 lenghtTrip".split("\\s+")).type);
         assertEquals(OptionType.exepensiveTrip, optionParser.parseOption("7 exepensiveTrip".split("\\s+")).type);
         assertEquals(OptionType.italy, optionParser.parseOption("7 italy".split("\\s+")).type);
+
+        try{
+            optionParser.parseOption("sum".split("\\s+"));
+            fail();
+        }catch (IllegalArgumentException e){
+            assertEquals(e.getMessage(),"Zbyt mało argumentów, minimalna liczba argumentów 2");
+        }
+
+        try{
+            optionParser.parseOption("sum bla".split("\\s+"));
+            fail();
+        }catch (IllegalArgumentException e){
+            assertEquals(e.getMessage(),"Nie poprawny argument : sum Powinien być numer kadencji");
+        }
+
+        try{
+            optionParser.parseOption("6 bla".split("\\s+"));
+            fail();
+        }catch (IllegalArgumentException e){
+            assertEquals(e.getMessage(),"Program obsluguje tylko 7 i 8 kadencje");
+        }
+
+        try{
+            optionParser.parseOption("7 bla".split("\\s+"));
+            fail();
+        }catch (IllegalArgumentException e){
+            String[] array = {"sumExepenses","smallExepense","avgExepenses","numberTrips","lenghtTrip","exepensiveTrip","italy"};
+            assertEquals(e.getMessage(),"Nie poprawny typ zapytania, poprawne typy: " + new LinkedList<>(Arrays.asList(array)));
+        }
+
     }
 
 }
